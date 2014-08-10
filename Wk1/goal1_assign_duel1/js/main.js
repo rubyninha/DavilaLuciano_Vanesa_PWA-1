1cbc9737d629affb9c16a43c2228af41b93a3ef3
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-08-10 19:08:22 -0400

    Initial setup of self-executing function in JS file

---
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -0,0 +1,14 @@
+/*
+ Name: Vanesa Dávila-Luciano
+ Date: 08/10/2013
+ Course: PWA I 1408 Section 01
+ Assignment: Goal 1: Assignment Duel 1
+ */
+
+
+// Self-executing function
+(function(){
+
+    console.log("FIGHT!!!");
+
+})();
