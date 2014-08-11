62e451e55adacc63d4051351dc30bb6c3a957a83
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-08-10 21:15:22 -0400

    Added correction to Pseudocode and comments in the JS code

--- Wk1/goal1_assign_duel1/js/main.js
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -9,26 +9,29 @@
 // Self-executing function
 (function(){
 
+    //Print out to console the word FIGHT
     console.log("FIGHT!!!");
 
-    //player name
+    //Create variables for players' names
     var playerOneName = "Spiderman";
     var playerTwoName = "Batman";
 
-    //player damage
+    //Create variables for players damage points
     var player1Damage = 50;
     var player2Damage = 50;
 
-    //player health
+    //Create variables for players health points
     var playerOneHealth = 100;
     var playerTwoHealth = 100;
 
-    //initiate round
-    var round=1;
+    //Create a variable to initiate round
+    var round=0;
 
     function fight(){
+        //Print to console to test function
         console.log("in the fight function");
 
+        //Alert to players to start the game
         alert(playerOneName+":"+playerOneHealth+"  *START*  "+playerTwoName+":"+playerTwoHealth);
 
         //Create for loop that will reduce the player's health for each round
@@ -45,29 +48,41 @@
             playerOneHealth-=f1;
             playerTwoHealth-=f2;
 
+            //Print to console
             console.log(playerOneName+":"+playerOneHealth+"  "+playerTwoName+":"+playerTwoHealth);
 
-            var result = winnerCheck();
-            console.log(result);
-
-
+            //Create  variable to give results
+            var results = winnerCheck();
+            //Print out results to console
+            console.log(results);
+            //Create conditional to determine the winner after the 10th round
+            if (results==="no winner"){
+                round++;
+                //Alert players results on each round
+                alert(playerOneName+":"+playerOneHealth+"  *ROUND "+round+" OVER"+"*  "+playerTwoName+":"+playerTwoHealth);
+            }else {
+                alert(results);
+                break;
+            };
         };
     };
 
     function winnerCheck(){
         console.log("in winnerCheck FN");
 
-        var result="no winner";
+        //Create variable if there is no winner
+        var results="no winner";
 
         //Create conditional to determine if there is a winner or not
         if (playerOneHealth<1 && playerTwoHealth<1){
-            result = "You Both Die";
+            results = "You Both Die";
         } else if(playerOneHealth<1){
-            result =playerTwoName+" WINS!!!"
+            results =playerTwoName+" WINS!!!"
         } else if (playerTwoHealth<1){
-            result = playerOneName+" WINS!!!"
+            results = playerOneName+" WINS!!!"
         };
-        return result;
+        //Return the results
+        return results;
     };
 
     //The program gets started below
