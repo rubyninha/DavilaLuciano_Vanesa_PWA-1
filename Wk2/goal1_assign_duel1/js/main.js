5fed674e985a15c2e80a1a73a391dca52f4db613
Vanesa Dávila-Luciano <rubyninha@gmail.com>
2014-08-15 22:38:45 -0400

    Recreated random formula using arrays to calculate damage to players' health

--- Wk2/goal1_assign_duel1/js/main.js
+++ Wk2/goal1_assign_duel1/js/main.js
@@ -32,15 +32,15 @@
         for (var i = 0; i < 10; i++){
 
             //random formula is - Math.floor(Math.random() * (max - min) + min);
-            var minDamage1 = player1Damage * .5;
-            var minDamage2 = player2Damage * .5;
-            var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
-            var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
+            var minDamage1 = fighter1[1] * .5;
+            var minDamage2 = fighter2[1] * .5;
+            var f1 = Math.floor(Math.random()*(fighter1[1]-minDamage1)+minDamage1);
+            var f2 = Math.floor(Math.random()*(fighter1[1]-minDamage2)+minDamage2);
 
 
             //inflict damage
-            playerOneHealth-=f1;
-            playerTwoHealth-=f2;
+            fighter1[2]-=f1;
+            fighter2[2]-=f2;
 
             //Print to console
             console.log(playerOneName+":"+playerOneHealth+"  "+playerTwoName+":"+playerTwoHealth);
